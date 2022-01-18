ea6dce6c8ea9ced1fb317938614fd61c859a64a7
Pedro Veiga <p.veiga@campus.fct.unl.pt>
2022-01-18 14:56:32 +0000

    Add features extraction with TSFEL

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import novainstrumentation as ni
+import tsfel
 
 
 def calculate_r(signal):
@@ -9,15 +10,53 @@ def calculate_r(signal):
     avg_ch10 = np.mean(signal[:, 3])
     ac_ch9 = signal[:, 2].max() - signal[:, 2].min()
     ac_ch10 = signal[:, 3].max() - signal[:, 3].min()
-    r = (ac_ch9/avg_ch9)/(ac_ch10/avg_ch10)
+    r = (ac_ch9 / avg_ch9) / (ac_ch10 / avg_ch10)
 
     return r
 
-path = 'data pedro/opensignals_spo2_pedro_2021-12-03_15-16-47.txt'
-raw_data = np.loadtxt(path)
-raw_data = pd.read_csv(path, delim_whitespace=True,\
-                       header=0, names=["nSeq", "DI", "CH9A", "CH9B", "%SpO2"],\
-                       skiprows=2, usecols=("nSeq", "CH9A", "CH9B", "%SpO2"))
+
+def is_rest(x):
+    return ((30 * fs < x) & (x < 60 * fs) |
+            (90 * fs < x) & (x < 120 * fs) |
+            (150 * fs < x) & (x < 180 * fs) |
+            (210 * fs < x) & (x < 240 * fs) |
+            (270 * fs < x) & (x < 300 * fs) |
+            (330 * fs < x) & (x < 360 * fs))
+
+
+def is_medium(x):
+    return ((60 * fs < x) & (x < 90 * fs) |
+            (120 * fs < x) & (x < 150 * fs) |
+            (180 * fs < x) & (x < 210 * fs))
+
+
+def is_high(x):
+    return ((240 * fs < x) & (x < 270 * fs) |
+            (300 * fs < x) & (x < 330 * fs) |
+            (360 * fs < x) & (x < 390 * fs))
+
+
+def get_features(df):
+    # Separate the diferent situations
+    df['state'] = 'default'
+    df['state'] = np.where(is_rest(df['nSeq']), 'rest_pressure', df['state'])
+    df['state'] = np.where(is_medium(df['nSeq']), 'medium_pressure', df['state'])
+    df['state'] = np.where(is_high(df['nSeq']), 'high_pressure', df['state'])
+
+    df = df.drop(df[df['state'] == 'default'].index)
+
+    data_separated = np.array_split(df, round(len(df) / (window_size * fs)))
+
+    # Write features
+    for block in data_separated:
+        features = tsfel.time_series_features_extractor(cfg, block[['CH9A', 'CH9B']], fs=fs)
+        features.to_csv(f, header=False, index=False, line_terminator=',')
+        f.write(block.iloc[0]['state'] + '\n')
+
+
+path_pedro = 'data pedro/opensignals_spo2_pedro_2021-12-03_15-16-47.txt'
+path_rodrigo = 'data pedro/opensignals_spo2_rodrigo_2021-12-03_14-57-47.txt'
+path_diogo = 'data pedro/opensignals_spo2_diogo_2021-12-03_15-08-49.txt'
 
 # "sampling rate": 1000,
 # "resolution": [16, 16],
@@ -26,54 +65,31 @@ raw_data = pd.read_csv(path, delim_whitespace=True,\
 # "label": ["CH9A", "CH9B", "%SpO2"],
 # "column": ["nSeq", "DI", "CH9A", "CH9B", "%SpO2"]
 
+fs = 1000
+window_size = 1
+cfg = tsfel.get_features_by_domain('temporal')
+f = open('features_temporal.csv', 'w')
 
-# fs = 1000
-#
-# rest1 = raw_data[30*fs:60*fs, :]
-# rest2 = raw_data[90*fs:120*fs, :]
-# rest3 = raw_data[150*fs:180*fs, :]
-# rest4 = raw_data[150*fs:180*fs, :]
-# rest5 = raw_data[210*fs:240*fs, :]
-# rest6 = raw_data[270*fs:300*fs, :]
-# rest7 = raw_data[330*fs:360*fs, :]
-# rest = np.concatenate((rest1, rest2, rest3, rest4, rest5, rest6, rest7))
-#
-# medium_pressure1 = raw_data[60*fs:90*fs, :]
-# medium_pressure2 = raw_data[120*fs:150*fs, :]
-# medium_pressure3 = raw_data[180*fs:210*fs, :]
-# medium_pressure = np.concatenate((medium_pressure1, medium_pressure2, medium_pressure3))
-#
-# high_pressure1 = raw_data[240*fs:270*fs, :]
-# high_pressure2 = raw_data[300*fs:330*fs, :]
-# high_pressure3 = raw_data[360*fs:390*fs, :]
-# high_pressure = np.concatenate((high_pressure1, high_pressure2, high_pressure3))
-#
-# block_size = 2 # ter cuidado para ser divisivel pelo tamanho do vetor
-#
-# data_rest = []
-#
-# for i in range(0, 30*7, block_size):
-#     data_rest.append(rest[i*fs: (i+block_size)*fs, :])
-#
-# data_medium = []
-# data_high = []
-# for i in range(0, 30*3, block_size):
-#     data_medium.medium_pressure(rest[i*fs: (i+block_size)*fs, :])
-#     data_high.high_pressure(rest[i*fs: (i+block_size)*fs, :])
-#
-# r_rest = []
-# r_medium = []
-# r_high = []
-# for block in data_rest: # em cada bloco de 3segundos
-#     r_rest.append(calculate_r(block))
-# for block in data_medium:
-#     r_medium.append(calculate_r(block))
-# for block in data_high:
-#     r_high.append(calculate_r(block))
-#
-# plt.plot(r_rest)
-# plt.figure()
-# plt.plot(r_medium)
-# plt.figure()
-# plt.plot(r_high)
-# plt.show()
+data_pedro = pd.read_csv(path_pedro, delim_whitespace=True,
+                         header=0, names=["nSeq", "DI", "CH9A", "CH9B", "SpO2"],
+                         skiprows=3, usecols=("nSeq", "CH9A", "CH9B", "SpO2"))
+data_rodrigo = pd.read_csv(path_rodrigo, delim_whitespace=True,
+                           header=0, names=["nSeq", "DI", "CH9A", "CH9B", "SpO2"],
+                           skiprows=3, usecols=("nSeq", "CH9A", "CH9B", "SpO2"))
+data_diogo = pd.read_csv(path_diogo, delim_whitespace=True,
+                         header=0, names=["nSeq", "DI", "CH9A", "CH9B", "SpO2"],
+                         skiprows=3, usecols=("nSeq", "CH9A", "CH9B", "SpO2"))
+
+# Write header
+temp = np.array_split(data_pedro, round(len(data_pedro) / (window_size * fs)))
+header_string = ''
+header = tsfel.time_series_features_extractor(cfg, temp[0][['CH9A', 'CH9B']], fs=fs)
+for col in header.columns:
+    header_string += col + ','
+f.write(header_string + 'state' + '\n')
+
+get_features(data_pedro)
+get_features(data_rodrigo)
+get_features(data_diogo)
+
+f.close()
